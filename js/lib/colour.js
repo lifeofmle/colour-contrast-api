b5affffb86465ffc8bae2c142df311549e5dbe4a
Michael Le <le.michael@gmail.com>
2015-10-22 23:37:59 +0100

    Start to create the library

--- js/lib/colour.js
+++ js/lib/colour.js
@@ -1,11 +1,46 @@
-var Colour = function(){
-	this.name = "Blue";
-};
 
-Colour.prototype = {
-	updateColour: function(val) {
-		this.name = val;
+(function() {
+
+	// Extend Math.round to allow for precision
+	Math.round = (function(){
+		var round = Math.round;
+		
+		return function (number, decimals) {
+			decimals = +decimals || 0;
+			
+			var multiplier = Math.pow(10, decimals);
+			
+			return round(number * multiplier) / multiplier;
+		};
+	})();
+
+	var Colour = (function() {
+		var Colour = function(rgba) {
+			this.name = rgba;      
+		};
+
+		Colour.prototype = {
+
+			get inverse () {
+				return 2;
+			},
+
+			updateColour: function(val) {
+				this.name = val;
+			},      
+
+			generate: function() {
+				return Math.round(this.inverse, 2);
+			}
+		};
+
+		return Colour;
+	})();
+
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){		
+		module.exports = Colour;
 	}
-}
-
-module.exports = Colour;
+	else{
+		window.Colour = Colour;
+	}
+})();
--- spec/colourSpec.js
+++ spec/colourSpec.js
@@ -1,39 +1,50 @@
-var colourLib = require("../js/lib/colour.js");
-var colour = new colourLib();
+var Colour = require("../js/lib/colour.js");
 
-describe("Colour Calculation", function() {
-	describe("Colour constrast calculation", function() {
+describe("Colour ratio", function() {
+	// convert hex to rgba
+	describe("function name", function() {
 		
-		it ("Colour initial", function() {
+		it ("scenario name", function() {
+			var colour = new Colour();
 			colour.updateColour("red");
 			var temp = colour.name;
 			
 			expect(temp).toBe("red");
 		});
 
-		// convert hex to rgba
+		it ("test generate", function() {
+			var colour = new Colour("test");
+			
+			expect(colour.name).toBe("test");
+		});
 
-		// check background is not empty
+	});		
 
-		// check foreground is not empty
+	// check background is not empty
+	describe("function name 2", function() {
+		
+		
+	});		
 
-		// check both empty
+	// check foreground is not empty
 
-		// check contrast ratio
-		// (L1 + 0.05) / (L2 + 0.05)
+	// check both empty
 
-		// brightness difference (>= 125)
-		// ((Red value * 299) + (Green value * 587) + (Blue value * 114)) / 1000
+	// check contrast ratio
+	// (L1 + 0.05) / (L2 + 0.05)
 
-		// colour difference (>= 500)
-		// (MAX(R1, R2)- MIN(R1, R2)) + (MAX(G1, G2)- MIN(G1, G2)) + (MAX(B1, B2)- MIN(B1, B2))
+	// brightness difference (>= 125)
+	// ((Red value * 299) + (Green value * 587) + (Blue value * 114)) / 1000
 
-		// WCAG 2 AA compliant
+	// colour difference (>= 500)
+	// (MAX(R1, R2)- MIN(R1, R2)) + (MAX(G1, G2)- MIN(G1, G2)) + (MAX(B1, B2)- MIN(B1, B2))
 
-		// WCAG 2 AA compliant (18pt+)
+	// WCAG 2 AA compliant
 
-		// WCAG 2 AAA compliant
+	// WCAG 2 AA compliant (18pt+)
 
-		// WCAG 2 AAA compliant (18pt+)
-	});
+	// WCAG 2 AAA compliant
+
+	// WCAG 2 AAA compliant (18pt+)
+	
 });
